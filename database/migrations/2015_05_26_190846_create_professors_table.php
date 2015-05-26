bc2cc3b76a67a7b69f2f1c590b89cf9c1b20f76a
Jefter Fontes <jefterlf@hotmail.com>
2015-05-26 16:40:49 -0400

    Subimdo o sistema

---
+++ database/migrations/2015_05_26_190846_create_professors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfessorsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('professors', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('Nome');
+			$table->string('Telefone');
+			$table->datetime('Aniversario');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('professors');
+	}
+
+}
