ea782b4c8942a9727c278de70cd2f80d6e0796a6
lucasbarque <lucasbarquedasilva@hotmail.com>
2015-06-01 20:12:53 -0300

    Revert "Adiciona campos na model Bimestre"
    This reverts commit 37d8d67bf42dd4eb984b6525c39d5b81894e667e.

--- app/Bimestre.php
+++ app/Bimestre.php
@@ -4,8 +4,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bimestre extends Model {
 
-	 protected $fillable = ['bimestre', 'data_inicio', 'data_final'];
-	 protected $guarded = ['id_bimestre'];
+	//teste
 
 }
-	
