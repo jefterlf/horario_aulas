480caf6b99159c7d45e9072cb690aa8e4f7d1544
lucasbarque <lucasbarquedasilva@hotmail.com>
2015-06-11 21:09:42 -0300

    Revert "Revert "Tradução formulário auth""
    This reverts commit 1bdbceda1b0bee2f4f66bd16c1115d6dac5c63e4.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,12 @@
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+				<div class="panel-heading">Início</div>
 
 				<div class="panel-body">
-					You are logged in!
+					Você está logado!
+                    <br/>
+                    <a class="btn btn-success" href="{!!URL::route('turmas_r.index')!!}"> Voltar</a></h1>
 				</div>
 			</div>
 		</div>
