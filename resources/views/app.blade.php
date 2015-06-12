e49f8d37418ba3813cf6b254054d950868474174
Jefter Fontes <jefterlf@hotmail.com>
2015-06-12 11:18:46 -0400

    ..

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -35,48 +35,51 @@
 	</style>
 </head>
 <body>
-
-<div id='cssmenu' style="-webkit-box-shadow: 0px 3px 5px 0px rgba(173,173,173,1);
--moz-box-shadow: 0px 3px 5px 0px rgba(173,173,173,1);
-box-shadow: 0px 3px 5px 0px rgba(173,173,173,1);">
-<ul>
-<span class="navbar-brand" href="#"> Escola Amélio</span>
-
-
-					@if (Auth::guest())
-					
-					<li><a href="{{ url('/') }}"><span class="glyphicon glyphicon-home"></span> Inicio</a></li>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+<div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Menu</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}"><span class="glyphicon glyphicon-home"></span> Controle de Horarios EEACB</a>
+</div>
+<div id="navbar" class="navbar-collapse collapse">
+	
+							@if (Auth::guest())
+					 <ul class="nav navbar-nav navbar-right">
+	           	 		<li><a href="{{ url('/auth/login') }}">Login</a></li>
+         		   </ul>
+							 @else
+            <ul class="nav navbar-nav">
+				
                     <li><a href="{!!URL::route('turmas_r.index')!!}"><span class="glyphicon glyphicon-tags"> Turmas</a></li>
                     <li><a href="{!!URL::route('horarios_r.index')!!}"><span class="glyphicon glyphicon-time"> Horários</a></li>
                     <li><a href="{!!URL::route('professors_r.index')!!}"><span class="glyphicon glyphicon-user"> Professores</a></li>
                     <li><a href="{!!URL::route('bimestres_r.index')!!}"><span class="glyphicon glyphicon-calendar"> Bimestres</a></li>
                     <li><a href="{!!URL::route('materias_r.index')!!}"><span class="glyphicon glyphicon-book"> Matérias</a></li>
-					
-						<li><a href="{{ url('/auth/login') }}">Login</a></li>
-						<li><a href="{{ url('/auth/register') }}">Register</a></li>
-					@else
-	
-						<li class='has-sub'>
-							<a href="#">{{ Auth::user()->name }} </a>
-							<ul class="dropdown-menu">
-								<li><a href="{{ url('/auth/logout') }}"><span class="glyphicon glyphicon-off"> Sair</a></li>
-							</ul>
-						</li>
-		 
-     
-					@endif
 
-
-</ul>
-				
+            </ul>
+			<ul class="nav navbar-nav navbar-right">
+			  <li><a href="{{ url('/auth/register') }}">Usuários</a></li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                	<li><a href="{{ url('/auth/logout') }}"><span class="glyphicon glyphicon-off"> Sair</a></li>
+                </ul>
+              </li>
+			  
+			 </ul>
+	@endif
 			
-				
-</div>
-				
 
+</div><!--/.nav-collapse -->
+</div><!--/.container-fluid -->
+</nav>
 
 @yield('content')
-	
 
 </body>
 </html>
