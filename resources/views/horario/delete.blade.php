c0d814b887c8fe5eceb723886abf5ef35340af41
jefersonlima <jefersonlimadesouza@hotmail.com.br>
2015-06-17 20:45:32 -0300

    config delete

--- resources/views/horario/delete.blade.php
+++ resources/views/horario/delete.blade.php
@@ -2,18 +2,31 @@
 @section('content')
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-          <h1>Horarios  <a class="btn btn-success" href="{!!URL::route('horarios_r.create')!!}">Novo +</a>    </h1>
+ <div class="col-md-8 col-md-offset-2">
+ <h1>Turma</h1>
       <div class="panel panel-default">
-        <div class="panel-heading">Cadastro</div>
+        
+       <div class="alert alert-danger" role="alert">
+       
+  {!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('horarios_r.destroy', $horario->id_horario))) !!}
+ <h4> Você realmente deseja deletar este registro?  
+                             {!! Form::submit('Deletar', array('class' => 'btn btn-danger')) !!}
+                         <input onclick="window.history.back();"  type="submit" class="btn btn-primary" value="Cancelar" />
+                    {!! Form::close() !!}
+
+      
+</h4>
+                        
+
+       </div>
+        <div class="panel-body">
+  
         <div class="panel-body">
 
-        	<div class="panel-body">
-
-         {!! Form::model($horario, array('route' => array('horarios_r.update', $horario->id_horario), 'class'=>'form-horizontal', 'method' => 'PUT')) !!}
-               
+       <form class="form-horizontal">
+                 <fieldset disabled>
               <div class="form-group">
-                <label  class="col-md-4 control-label" for="dia_semana">Dia semana:</label>
+                <label  class="col-md-4 control-label" for="dia_semana">Dia Semana:</label>
                 <div class="col-md-6">
                    <input class="form-control" type="text" name="dia_semana" value="{{ $horario->dia_semana }}"> 
                  </div>
@@ -28,34 +41,27 @@
                    <label class="col-md-4 control-label"  for="id_turma">Turma:</label>
                  <div class="col-md-6">
                   
-                     <?php echo Form::select('id_turma', array('0' => 'Selecione') + $turmas, $horario->id_turma, array('class' => 'form-control'));?>
-            																			
+                     <?php echo Form::select('id_bimestre', array('0' => 'Selecione') + $turmas, $horario->id_turma, array('class' => 'form-control'));?>
+            
                    
                  </div>
                </div>
          
-                <input type="hidden" name="_token" id="csrf-token" value="{{ Session::token() }}" />
-                <div class="form-group">
-                <div class="col-md-6 col-md-offset-4">
-                  <input type="submit" class="btn btn-primary" value="Salvar" />
-                </div>
-                </div>
-   
-  {!! Form::close() !!}
-      </div>
-          
-      </div>
+
+   </fieldset>
+  </form>
+
+
       </div>
     </div>
   </div>
 </div>
 
+</div>
 <nav>
   <ul class="pager">
     <li onclick="window.history.back();" ><a href="" >Voltar</a></li>
   </ul>
 </nav>
-
-</div>
-
+</div>.
 @endsection
