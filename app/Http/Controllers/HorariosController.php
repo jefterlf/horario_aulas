eadfc2ea944b2c442f6968b5d02de25345a9d1e2
jefersonlima <jefersonlimadesouza@hotmail.com.br>
2015-06-17 20:21:31 -0300

    Bloqueando acesso

--- app/Http/Controllers/HorariosController.php
+++ app/Http/Controllers/HorariosController.php
@@ -14,6 +14,9 @@ class HorariosController extends Controller {
 	 *
 	 * @return Response
 	 */
+
+	public function __construct() { $this->middleware('auth'); } //Se o usuário não estiver logado, redireciona para a página de login
+	
 	public function index()
 	{
 		$horarios = Horario::all();
